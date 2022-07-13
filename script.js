8c4fc4da0026459e272608c88fea2d6282c96f5b
rubinduck <rubinduck@protonmail.com>
2022-07-13 03:51:57 +0300

    Add handling of enter action

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const CALCULATOR_CLASS_NAME = 'calculator';
 const INPUT_FIELD_CLASS_NAME = 'calculator__input-field';
 const INPUT_BUTTON_CLASS_NAME = 'input-button';
+const ENTER_BUTTON_CLASS_NAME = 'enter-button';
 
 const getElementsByClassName = (parent, className) => 
     Array.from(parent.getElementsByClassName(className));
@@ -22,11 +23,18 @@ class Calculator {
         const inputButtons = getElementsByClassName(calculatorElement, INPUT_BUTTON_CLASS_NAME);
         inputButtons.forEach(
             button => button.addEventListener('click', (e) => this.handleInputButtonClick(e)));
+
+        const enterButton = getElementsByClassName(calculatorElement, ENTER_BUTTON_CLASS_NAME)[0];
+        enterButton.addEventListener('click', (e) => this.handleEnter());
     }
 
     handleInputButtonClick(event){
         const button = event.currentTarget;
         this.inputField.value += button.textContent;
+    }
+
+    handleEnter(){
+        this.inputField.value = this.computeFunction(this.inputField.value);
     }
 }
 
